bf1dcbd6a0d9832968540f71b8c324d6dfd3f24e
coopnet-model <65873785+coopnet-model@users.noreply.github.com>
2020-05-24 16:23:56 -0700

    Create utils.py

---
+++ src/gen/utils.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import json
+import time
+import numpy as np
+from tqdm import tqdm
+
+def encode_dataset2(*splits, encoder):
+    encoded_splits = []
+    for split in splits:
+        fields = []
+        for field in split:
+            if isinstance(field[0], str):
+                field = [encoder.encode(x) for x in field]
+            fields.append(field)
+        encoded_splits.append(fields)
+    return encoded_splits
+
+def make_path(f):
+    d = os.path.dirname(f)
+    if d and not os.path.exists(d):
+        os.makedirs(d)
+    return f
+
+class ResultLogger(object):
+    def __init__(self, path, *args, **kwargs):
+        if 'time' not in kwargs:
+            kwargs['time'] = time.time()
+        self.f_log = open(make_path(path), 'w')
+        self.f_log.write(json.dumps(kwargs)+'\n')
+
+    def log(self, **kwargs):
+        if 'time' not in kwargs:
+            kwargs['time'] = time.time()
+        self.f_log.write(json.dumps(kwargs)+'\n')
+        self.f_log.flush()
+
+    def close(self):
+        self.f_log.close()
+
+def iter_data(*datas, n_batch=128, truncate=False, verbose=False, max_batches=float("inf")):
+    n = len(datas[0])
+    if truncate:
+        n = (n//n_batch)*n_batch
+    n = min(n, max_batches*n_batch)
+    n_batches = 0
+    if verbose:
+        f = sys.stderr
+    else:
+        f = open(os.devnull, 'w')
+    for i in tqdm(range(0, n, n_batch), total=n//n_batch, file=f, ncols=80, leave=False):
+        if n_batches >= max_batches: raise StopIteration
+        if len(datas) == 1:
+            yield datas[0][i:i+n_batch]
+        else:
+            yield (d[i:i+n_batch] for d in datas)
+        n_batches += 1
